d1b8a8aa650ce6bb1a4b88c6ab98f4510210e962
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-16 23:51:11 +0530

    Update Subcurrency.sol

--- Subcurrency.sol
+++ Subcurrency.sol
@@ -1,3 +1,9 @@
+/*The following contract implements the simplest form of a cryptocurrency.
+The contract allows only its creator to create new coins (different issuance schemes are possible).
+Anyone can send coins to each other without a need for registering with a username and password,
+all you need is an Ethereum keypair.*/
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
