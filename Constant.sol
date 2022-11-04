54c5f66ab57b0415f2cffb3aa73c8f7b2e5396d0
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-11-04 16:02:50 +0530

    Create Constant.sol

---
+++ Constant.sol
@@ -0,0 +1,15 @@
+
+/*Constants are variables that cannot be modified.
+
+Their value is hard coded and using constants can save gas cost.*/
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+    uint public constant number=21;
+    address public constant my_address=0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+
+
+}
